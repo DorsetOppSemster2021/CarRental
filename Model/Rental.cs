9d74e73b4f866372bd7e3c396ae790c8158f6c12
John Rowley <outsidethebox@eircom.net>
2021-11-23 17:22:06 +0000

    Add project files.

---
+++ Model/Rental.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Model
+{
+    public class Rental
+    {
+        public int RentalId { get; set; }
+        public string CarModel { get; set; }
+
+        public string CarRegistration { get; set; }
+
+        public Rental(int _rentalId, string _carModel, string carRegistration)
+        {
+            RentalId = _rentalId;
+            CarModel = _carModel;
+            CarRegistration = carRegistration;
+        }
+
+        public Rental()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using CarRental.Model;
+using System.Linq;
+namespace CarRental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarRentalItem carRentalItem = new CarRentalItem();
+
+            carRentalItem.ListOfCarRentals.Add(new Rental(5, "Ferrari", "a1234"));
+            carRentalItem.ListOfCarRentals.Add(new Rental(6, "Audi", "a1234"));
+            carRentalItem.ListOfCarRentals.Add(new Rental(7, "BMW", "a1234"));
+
+           
+
+            // carRentalItem.ListOfCarRentals.Sort(x => x.CarRegistration)
+
+            //  carRentalItem.DisplayRentals();
+            Console.WriteLine("===========================================");
+            Rental newRental = new Rental();
+            Console.WriteLine("Enter Car Rental Id");
+
+            // newRental.RentalId =  Int32.Parse(Console.ReadLine());
+            newRental.RentalId =  Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Car Rental Model");
+
+            newRental.CarModel = Console.ReadLine();
+
+
+            Console.WriteLine("Enter Car Rental Reg");
+
+            newRental.CarRegistration = Console.ReadLine();
+
+
+            carRentalItem.ListOfCarRentals.Add(newRental);
+
+
+            carRentalItem.DisplayRentals();
+
+        }
+    }
+}
